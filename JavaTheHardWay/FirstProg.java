a825678fcdc2bb4b4b3d62518f736f8946ded5c3
newel <newel_21@yahoo.com>
2018-08-07 08:55:19 -0500

    Completed: JTHW Ex 1

---
+++ JavaTheHardWay/FirstProg.java
@@ -0,0 +1,6 @@
+public class FirstProg{
+  public static void main( String[] args ) {
+    System.out.println( "I, Newel, am determined to learn how to code" );
+    System.out.println( "Today's date is 07 August 2018" );
+  }
+}
