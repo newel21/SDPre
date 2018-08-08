39b7a3b6c9e94e2fad777afb65f673cea06976f1
newel <newel_21@yahoo.com>
2018-08-08 11:06:23 -0500

    Completed: LABS Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,14 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name, state;
+    int age;
+
+    name = "Newel Miole";
+    state = "Hawaii";
+    age = 30;
+    System.out.print("Name: " + name);
+    System.out.print(" Age: " + age);
+    System.out.print(" State: " + state);
+    System.out.println();
+  }
+}
