9b50f0db0b25b3010f20214cb3696742de344f84
newel <newel_21@yahoo.com>
2018-08-10 18:57:14 -0500

    Completed: LABS Objective3Lab1

---
+++ Labs/Objective3Lab1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Objective3Lab1 {
+  public static void main(String[] args) {
+    String userInput;
+
+    Scanner keyboard = new Scanner(System.in);
+
+    System.out.println("Please Type a word: ");
+    userInput = keyboard.next();
+    System.out.println(userInput);
+
+    //input.close();
+  }
+}
