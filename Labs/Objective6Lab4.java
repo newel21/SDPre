b585913026b6ba3f5716efe57494fbeda20291e3
newel <newel_21@yahoo.com>
2018-08-24 07:04:36 -0600

    Completed: LABS Objective6Lab4

---
+++ Labs/Objective6Lab4.java
@@ -0,0 +1,13 @@
+public class Objective6Lab4 {
+  public static void main(String[] args) {
+    int counter = 1;
+    int total = 0;
+
+    while (counter <= 20) {
+      total += counter;
+      counter++;
+    }
+
+    System.out.println(total);
+  }
+}
